54dec0992b6f3d190f5ffb943b464fd3db9ec97b
P. Julius <87507466+pjmawuli@users.noreply.github.com>
2024-10-01 03:31:43 +0000

    Complete 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    if (num >= 0) {
+        new_text = ""
+        for (let count = 0; count < num; count++) {
+            new_text += text
+        }
+        return new_text
+    } else {
+        return "ERROR"
+    }
+    
 };
 
 // Do not edit below this line
