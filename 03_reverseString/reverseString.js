da13d5da0245b060cdb67aacf5dc89095208bd07
P. Julius <87507466+pjmawuli@users.noreply.github.com>
2024-10-01 04:09:50 +0000

    Complete 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    textList = text.split("")
+    reversed_text = ""
 
+    for (let i = textList.length - 1; i >= 0; i--) {
+        reversed_text += textList[i] 
+    }
+    return reversed_text
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
