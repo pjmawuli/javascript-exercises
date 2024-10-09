b0e16f73e89d20e89ee8c149bfe12d017ede2cb3
P. Julius <87507466+pjmawuli@users.noreply.github.com>
2024-10-09 22:16:34 +0000

    Complete removeFromArray Exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+function removeFromArray(list, ...rest) {
+  for (let i = 0; i < rest.length; i++) {
+    list = list.filter(num => num !== rest[i]);
+  };
+  return list;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
