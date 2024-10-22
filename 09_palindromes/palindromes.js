b6a5d4d7513f3fb3e2642c1002deb49c3f713042
P. Julius <87507466+pjmawuli@users.noreply.github.com>
2024-10-22 23:02:45 +0000

    Completed exercise on palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function(string_val) {
+  string_val = string_val.toLowerCase(); // you know what this does
+  let reg = /[\w\d]/g; // going to use regular expressions for this exercise
+  let arr = string_val.match(reg);
+  let arr_rev = [];
+
+  for (let index = arr.length - 1; index >= 0; index--) {
+    arr_rev.push(arr[index]);
+  }
+
+  // join the array back into a string
+  arr_rev = arr_rev.join("");
+  arr = arr.join("");
+
+  // print these for debugging purposes
+  console.log(arr);
+  console.log(arr_rev);
+  return arr_rev === arr;
 
 };
+
+console.log(palindromes("Racecar"));
 
 // Do not edit below this line
 module.exports = palindromes;
